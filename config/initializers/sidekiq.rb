94d667a9b28146c1be207a965fed6d0e4edb3cb4
VishalT312 <vt444713@gmail.com>
2026-05-10 10:39:54 +0530

    Extend cron time

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -12,7 +12,7 @@ Sidekiq.configure_server do |config|
   config.on(:startup) do
     Sidekiq::Cron::Job.create(
       name:  "Schedule all watchers every 30 minutes",
-      cron:  "*/30 * * * *",
+      cron:  "0 * * * *",
       class: "ScheduleWatchersJob"
     )
 
